f16c34361fb2f635827cafcde89613abc26f9568
aksoff <an_popoff@inbox.ru>
2020-05-26 15:13:53 +0400

    created 1 26/05/20 by aksoff

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="red darken-4">
+        <div class="nav-wrapper container">
+            <a href="#" class="brand-logo">Tasks</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <router-link
+                    to="/"
+                    tag="li"
+                >
+                    <a href="#">Create</a>
+                </router-link>
+                <router-link
+                    to="/List"
+                    tag="li"
+                >
+                    <a href="#">List</a>
+                </router-link>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-6 offset-6">
+      <h2>Create</h2>
+      <form>
+        <div class="input-field col s6">
+          <input placeholder="Title" id="title" type="text" class="validate" required>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Create',
+}
+</script>
